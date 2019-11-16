5794775d82eac3e284906707fbdeb9ab2cdff97d
liuyongjie <liuyj@chances.com.cn>
2019-11-16 14:37:09 +0800

    处理日志开启

--- app/src/main/java/com/runningsnail/miweather/MiApplication.java
+++ app/src/main/java/com/runningsnail/miweather/MiApplication.java
@@ -2,6 +2,8 @@ package com.runningsnail.miweather;
 
 import androidx.multidex.MultiDexApplication;
 
+import com.runningsnail.base.log.HiLogger;
+
 /**
  * @author yongjie created on 2019-11-16.
  */
@@ -9,5 +11,7 @@ public class MiApplication extends MultiDexApplication {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		//处理日志是否开启
+		HiLogger.setLogEnable(BuildConfig.LOG_ENABLE);
 	}
 }
